3f3773f5a3faafc23cf9107fc6069cbf303ca992
Pan Ziyue <random.rrr3@gmail.com>
2014-05-22 11:40:16 +0800

    edited both detail view controllers to automatically scale for iPad

--- SST Open House 2014/SubjectVCs/Detail2ViewController.m
+++ SST Open House 2014/SubjectVCs/Detail2ViewController.m
@@ -9,6 +9,9 @@
 #import "Detail2ViewController.h"
 
 @interface Detail2ViewController ()
+{
+    BOOL iPad;
+}
 
 @end
 
@@ -30,6 +33,11 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
+    if (UI_USER_INTERFACE_IDIOM()==UIUserInterfaceIdiomPad)
+        iPad=YES;
+    else
+        iPad=NO;
+    
     [self setTextAccordingToIdentifier:identifier];
 }
 
@@ -38,26 +46,38 @@
     if (ident==0) {
         // Introduction
         _textView.text=@"Ngee Ann Polytechnic will be offering applied subjects to upper secondary students at SST from 2012. The subjects which are designed, conducted and assessed by Ngee Ann Polytechnic are equivalent in rigour and scope to existing ‘O’ level subjects. The applied subject results will be reflected in the GCE ‘O’ level Examination Certificate and recognised for admission into Junior Colleges and Polytechnics.\n\nThe 4 applied subjects offered in 2013 are Biotechnology, Media Studies (EL), Fundamentals of Electronics and Design Studies. The school will continue to explore with MOE and the tertiary partners to offer other applied subjects in years to come. ";
-        _imageView.image = [self imageByScalingAndCroppingForSize:CGSizeMake(640, 480) withImage:[UIImage imageNamed:@"English"]];
+        if (iPad)
+            _imageView.image = [UIImage imageNamed:@"Introduction"];
+        else
+            _imageView.image = [self imageByScalingAndCroppingForSize:CGSizeMake(640, 480) withImage:[UIImage imageNamed:@"Introduction"]];
         self.title=@"Introduction";
     }
     else if (ident==1) {
         // Biotechnology
         _textView.text=@"The Biotechnology course introduces students to the study of the use of living organisms or their products, and cellular biomolecular processes, to solve problems or make useful products for the benefit of mankind. The subject uses knowledge from a variety of disciplines including, biology, chemistry, physics, bioinformatics, genetic engineering and manufacturing processes. The subject of biotechnology is, however, distinct from biology as while biology studies basic processes of living organisms, biotechnology is the application of biology to solve problems and improve lives.\n\nStudents will have an opportunity to learn about principles and applications of various areas of biotechnology such as cell culture technology, microbial biotechnology and recombinant DNA technology. This subject helps the students develop good laboratory skills and a research mindset. It also provides students with a foundation for further studies in biotechnology, biomedical science and other related fields.";
-        _imageView.image = [self imageByScalingAndCroppingForSize:CGSizeMake(640, 480) withImage:[UIImage imageNamed:@"Biotechnology"]];
+        if (iPad)
+            _imageView.image = [self imageByScalingAndCroppingForSize:CGSizeMake(768, 576) withImage:[UIImage imageNamed:@"Biotechnology"]];
+        else
+            _imageView.image = [self imageByScalingAndCroppingForSize:CGSizeMake(640, 480) withImage:[UIImage imageNamed:@"Biotechnology"]];
         self.title=@"Biotechnology";
     }
     else if (ident==2) {
         // FOE
         _textView.text=@"The Fundamentals of Electronics course aims to equip students with a sound understanding of basic electronics theory and at the same time provide ample opportunities for students to hone their creative and problem solving skills through practical application of electronics to the design of authentic gadgets and products.\n\nThe topics taught include fundamentals of physics and electricity, types of resistors, voltage and current sources, Network Theorem and analysis, the capacitor, light emitting semiconductors, sensors, and output devices, The Bipolar Junction Transistor, and project management.\n\nStudents will experience motivating learning through real world applications such as building of a transistor organ, rain detector, sound activated ballet dancer, universal electronic timer, heat sensor, line-follower robot, and a two-way electronic intercom system.\n\nStudents will hone their creative and problem-solving skills through effective training in basic electronics theory and practical electronics.";
-        _imageView.image = [self imageByScalingAndCroppingForSize:CGSizeMake(640, 480) withImage:[UIImage imageNamed:@"FOE"]];
+        if (iPad)
+            _imageView.image = [self imageByScalingAndCroppingForSize:CGSizeMake(768, 576) withImage:[UIImage imageNamed:@"FOE"]];
+        else
+            _imageView.image = [self imageByScalingAndCroppingForSize:CGSizeMake(640, 480) withImage:[UIImage imageNamed:@"FOE"]];
         self.title=@"F.O.E.";
     }
     else if (ident==3) {
         // Design Studies
         
         _textView.text=@"The Design Studies course equips students with the knowledge of the characteristics of the design field via exposure to design thinking, the design process, and the fundamentals of three design disciplines: that of visual communication design, interior and exhibition space design, and architectural design.\n\nThe subject aims to nurture an awareness and appreciation of the positive relationships spanning design, humanity, art, technology, business, culture and economic development; and the role of design in shaping one’s experience of visual communication, space, objects and the environment.\n\nStudents will develop creative and innovative mindsets, critical thinking and analytical skills through design activities; and acquire the ability to critique design works and to generate creative design solutions. Students will also acquire competencies for presenting design concepts and effective oral communication through innovative technology use.";
-        _imageView.image = [self imageByScalingAndCroppingForSize:CGSizeMake(640, 480) withImage:[UIImage imageNamed:@"DS"]];
+        if (iPad)
+            _imageView.image = [self imageByScalingAndCroppingForSize:CGSizeMake(768, 576) withImage:[UIImage imageNamed:@"DS"]];
+        else
+            _imageView.image = [self imageByScalingAndCroppingForSize:CGSizeMake(640, 480) withImage:[UIImage imageNamed:@"DS"]];
         self.title=@"Design Studies";
     }
     _textView.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
--- SST Open House 2014/SubjectVCs/DetailViewController.m
+++ SST Open House 2014/SubjectVCs/DetailViewController.m
@@ -10,6 +10,9 @@
 #import "UIImage-Extensions.h"
 
 @interface DetailViewController ()
+{
+    BOOL iPad;
+}
 
 -(void)setTextAccordingToIdentifier:(NSUInteger)identifier;
 
@@ -33,6 +36,11 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+        iPad=YES;
+    else
+        iPad=NO;
+    
     [self setTextAccordingToIdentifier:identifier];
 }
 
@@ -41,41 +49,60 @@
     if (ident==0) {
         // English
         _textView.text=@"Language is functional as it is used to make and exchange meanings influenced by social and cultural contexts. \nLocally, the English language is used to facilitate communication and bonding among the different ethnic and cultural groups. \nGlobally, it allows Singaporeans to participate actively and effectively in a knowledge-based economy where it is the lingua franca of the Internet, of science and technology, and of world economy.\n\nLearning Outcomes:\n\nAt the end of four years, a SST graduate who has mastered the English Language will be able to:\n\nCommunicate fluently and effectively using appropriate text types to achieve the purpose, context, culture and audience intended.\nEmploy critical and creative thinking skills in evaluating arguments and opinions and generating ideas.\nAppreciate the nuances in language as used by writers in varying texts.\nUnderstand the relationships between ideas within and across disciplines.\nUse Information and Communication Technology actively and purposefully to facilitate the understanding, learning and use of the English Language.";
-        _imageView.image = [self imageByScalingAndCroppingForSize:CGSizeMake(640, 480) withImage:[UIImage imageNamed:@"English"]];
+        if (iPad)
+            _imageView.image = [self imageByScalingAndCroppingForSize:CGSizeMake(768, 576) withImage:[UIImage imageNamed:@"English"]];
+        else
+            _imageView.image = [self imageByScalingAndCroppingForSize:CGSizeMake(640, 480) withImage:[UIImage imageNamed:@"English"]];
         self.title=@"English";
     }
     else if (ident==1) {
         // Maths
         _textView.text=@"The nature of mathematics serves to conceptualise the world and the relationships contained within, as it searches for patterns to simplify every day life and the issues inherent within - a process made simpler through Multi-Modal Mathematical Representations.\n\nIt presents an excellent platform for nurturing a student’s tenacity, inquisitiveness and curiosity, risk-taking, critical and creative thinking; for enhancing a student’s appreciation, understanding and application of mathematical concepts for effective problem solving in real world context and within the structures of formal Mathematics; and for developing individuals who actively seek real-world significance of their learning.\n\nLearning Outcomes\n\nThe SST Mathematics programme seeks to nurture innovative individuals able to lead in the forefront of science and technology, and who are able to:\n\nAppreciate the beauty and elegance of mathematics\nCommit to continual learning of mathematics\nEffectively communicate and reason mathematically\nMake connections between mathematics and its applications both within the real-world and theoretical context\nUse mathematics to effectively contribute to society\nUse technology to triangulate learning across the 3 main representations of Mathematics – PROCEDURAL, GRAPHICAL and NUMERICAL.";
-        _imageView.image = [self imageByScalingAndCroppingForSize:CGSizeMake(640, 480) withImage:[UIImage imageNamed:@"Maths"]];
+        if (iPad)
+            _imageView.image = [self imageByScalingAndCroppingForSize:CGSizeMake(768, 576) withImage:[UIImage imageNamed:@"Maths"]];
+        else
+            _imageView.image = [self imageByScalingAndCroppingForSize:CGSizeMake(640, 480) withImage:[UIImage imageNamed:@"Maths"]];
+        
         self.title=@"Mathematics";
     }
     else if (ident==2) {
         // Science
         _textView.text=@"Science furthers the understanding of our natural and physical worlds as well as the wider universe through the process of scientific inquiry; which involves generating ideas, testing hypotheses, gathering evidence through observations, and conducting investigations and modelling. Through the study of Science, our students will become confident citizens in a technological world, able to take or develop an informed interest in matters of scientific importance.\n\nLearning Outcomes\n\nBy the end of four years, a SST graduate will be able to:\n\nUnderstand and apply major concepts in biology, chemistry and physics\nDevelop an understanding of the natural and physical worlds, built on current scientific theories and apply what they have learnt to authentic solutions\nEngage in the process of scientific inquiry\nApply critical and creative thinking to solve problems and create new knowledge\nBecome more aware of ethical and moral issues related to scientific advances.";
-        _imageView.image = [self imageByScalingAndCroppingForSize:CGSizeMake(640, 480) withImage:[UIImage imageNamed:@"Science"]];
+        if (iPad)
+            _imageView.image = [self imageByScalingAndCroppingForSize:CGSizeMake(768, 576) withImage:[UIImage imageNamed:@"Science"]];
+        else
+            _imageView.image = [self imageByScalingAndCroppingForSize:CGSizeMake(640, 480) withImage:[UIImage imageNamed:@"Science"]];
+        
         self.title=@"Applied Science";
     }
     else if (ident==3) {
         // IH
         
         _textView.text=@"The study of the Humanities is the exploration of the human condition and experience, as they are impacted on by the geographical, environmental, political, economic, and social systems of the time and place.  The understanding and application of the knowledge, skills and values across History, Geography and Social Studies will provide our students a better appreciation and understanding of their inter-connectedness to progress or hinder human development and growth over cultures, time and space.\n\nLearning Outcomes\n\nStudents will acquire knowledge about:\n\nFundamental Humanities concepts, e.g. cause and consequence, change and continuity, space and place, relationships, systems and interconnections, and scarcity and choice\nSocieties and their cultures and how these impact human behaviors and actions\nKey personalities and their contributions and their impact on the world and the evolution of the Singapore narrative\nHow major events are related to one another in time.\n\nStudents will be able to:\n\nDevelop the skills of historical and geographical investigations/inquiry by asking meaningful questions, planning their own process for learning and searching for answers\nInterpret a variety of primary and secondary sources, e.g. maps, statistics, documents, media and audio clips, photographs\nCritically analyse and evaluate sources and data from different perspectives so as to arrive at an informed conclusion to an issue or problem\nPresent points of view or solutions confidently using appropriate platforms; and demonstrate the ability to work independently and collaboratively.";
-
-        _imageView.image = [self imageByScalingAndCroppingForSize:CGSizeMake(640, 480) withImage:[UIImage imageNamed:@"IH"]];
+        if (iPad)
+            _imageView.image = [self imageByScalingAndCroppingForSize:CGSizeMake(768, 576) withImage:[UIImage imageNamed:@"IH"]];
+        else
+            _imageView.image = [self imageByScalingAndCroppingForSize:CGSizeMake(640, 480) withImage:[UIImage imageNamed:@"IH"]];
         self.title=@"Humanities";
     }
     else if (ident==4) {
         // IRS
         
         _textView.text=@"Being able to effectively seek and create knowledge through applying the research process is essential for preparing students for life-long learning and a life of contribution to the knowledge community.  SST believes that students should be equipped with relevant skills and knowledge necessary for research projects from a young age.\n\nBy having our students conduct authentic research in and out of the classroom, they will be able to continue their search for knowledge in a systematic manner underpinned by ethical and social responsibility in the use, interpretation and generation of new knowledge.\n\nThe interdisciplinary nature of research done in authentic settings will aid students in transferring knowledge across disciplines and into the real world.\n\nLearning Outcomes\n\nStudents will be able to:\nAcquire project skills as a precursor to research and problem solving skills\nApply knowledge and skills across disciplines in conducting their research tasks\nApply the processes and tools of research to deepen their understanding of or solve a real world issue/problem\nFormulate clear and precise research questions, select appropriate research methodology, gather and analyse data, reach conclusions based on evidence, write research reports, and present their findings.\nDevelop skills and dispositions that foster life-long learning.";
-        _imageView.image = [self imageByScalingAndCroppingForSize:CGSizeMake(640, 480) withImage:[UIImage imageNamed:@"IRS"]];
+        if (iPad)
+            _imageView.image = [self imageByScalingAndCroppingForSize:CGSizeMake(768, 576) withImage:[UIImage imageNamed:@"IRS"]];
+        else
+            _imageView.image = [self imageByScalingAndCroppingForSize:CGSizeMake(640, 480) withImage:[UIImage imageNamed:@"IRS"]];
         self.title=@"Research Studies";
     }
     else if (ident==5) {
         // ICT
         
         _textView.text=@"The emergence of modern technology has forever changed the way we live, learn, work and play. Information and Communication Technology (ICT) is a leveller of the playing field for many in education, businesses, entertainment and politics, and as such, its effective and efficient use is of great importance for all students, especially SST students with their 1:1 learning devices.  Explicit instruction in ICT knowledge and skills must therefore be a part of the school curriculum for our students.\n\nLearning Outcomes\n\nStudents will be able to:\n\nUse their learning devices and multimedia software effectively to enhance and extend their learning in all subject areas.\nUtilise online tools for communication, learning and programming\nProgram in languages such as C++ and Objective-C";
-        _imageView.image = [self imageByScalingAndCroppingForSize:CGSizeMake(640, 480) withImage:[UIImage imageNamed:@"ICT"]];
+        if (iPad)
+            _imageView.image = [self imageByScalingAndCroppingForSize:CGSizeMake(768, 576) withImage:[UIImage imageNamed:@"ICT"]];
+        else
+            _imageView.image = [self imageByScalingAndCroppingForSize:CGSizeMake(640, 480) withImage:[UIImage imageNamed:@"ICT"]];
         self.title=@"Infocomm Tech";
     }
     _textView.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
